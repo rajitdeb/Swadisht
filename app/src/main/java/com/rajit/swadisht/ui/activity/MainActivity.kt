716ca6263a9ec2cb6e522e72408bdb7f47b85a9f
Rajit Deb <rajitdeb456@gmail.com>
2023-05-14 14:46:36 +0530

    Implemented Splash Screen using SplashScreenAPI

--- app/src/main/java/com/rajit/swadisht/ui/activity/MainActivity.kt
+++ app/src/main/java/com/rajit/swadisht/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.rajit.swadisht
+package com.rajit.swadisht.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.rajit.swadisht.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
